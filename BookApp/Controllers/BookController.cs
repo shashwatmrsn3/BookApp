0ab3baac8fe100187131ce2f73015341e367211c
shashwatmrsn <shashwatmrsn3@gmail.com>
2023-01-03 12:50:27 +0545

    Add project files.

---
+++ BookApp/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookApp.Models;
+
+namespace BookApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Create(BookModel bookModel)
+        {
+            Console.WriteLine(bookModel.Name);
+            Console.WriteLine(bookModel.Author);
+            Console.WriteLine(bookModel.ISBN);
+          
+        }
+
+
+    }
+}
---
+++ BookApp/Models/BookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BookApp.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
